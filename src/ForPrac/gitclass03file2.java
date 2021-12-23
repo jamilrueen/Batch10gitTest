ddf98beb8c6b8410ea92fbf0e5a8fd486b1878f8
abduljamil <jamil.rueen58@gmail.com>
2021-12-23 01:19:03 -0500

    Latest Update

--- src/ForPrac/gitclass03file2.java
+++ src/ForPrac/gitclass03file2.java
@@ -3,7 +3,7 @@ package ForPrac;
 public class gitclass03file2 {
 
 	public static void main(String[] args) {
-		System.out.println("This is the second commit");
+		System.out.println("This is the second commit. Moazzam'S changes");
 
 	}
 
